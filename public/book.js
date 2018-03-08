a0144cb6beb26b6190ec03c17bdeedaa271077cf
Joe Rezendes <rezenbiz@gmail.com>
2018-03-08 15:27:26 -0800

    Book of life v 1

--- public/book.js
+++ public/book.js
@@ -3,15 +3,36 @@ $(document).ready(function()
 
 $('.pg2').on("click", function() {
 
-  $(this).addClass('animated flip');
+  $(this).css('transform', 'rotateY(-180deg)');
 
 })
 
-$(function() {
+$('.pg3').on("click", function() {
 
-	$( '#bb-bookblock' ).bookblock();
+   $(this).css('transform', 'rotateY(-180deg)');
+   $(this).css('z-index', '20');
 
-});
+})
+
+$('.pg4').on("click", function() {
+
+   $(this).css('transform', 'rotateY(-180deg)');
+   $(this).css('z-index', '20');
+
+})
+
+$('.pg5').on("click", function() {
+
+$('.book').css("transform", "rotateY(40deg) rotateX(-39deg) scale(1.1)");
 
 
-});
+})
+
+
+
+
+// $('.pg1').hover(
+//        function(){ $(this).addClass('animated flip') },
+//        function(){ $(this).removeClass('animated flip') }
+// )
+})
