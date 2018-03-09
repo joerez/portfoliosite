a0f0c1c0fb5595e58e2c952135fee80dde72de90
Joe Rezendes <rezenbiz@gmail.com>
2018-03-09 15:44:08 -0800

    Better book

--- public/book.js
+++ public/book.js
@@ -28,6 +28,13 @@ $('.book').css("transform", "rotateY(40deg) rotateX(-39deg) scale(1.1)");
 
 })
 
+$('.cover').on("click", function() {
+
+  $('.book').css("transform", "rotateY(0deg) rotateX(-0deg) scale(1)");
+   $('.cover').css('transform', 'rotateY(-180deg)').css("color", "brown");
+   $(this).css('z-index', '20');
+   $('.firstPage').delay(100).show();
+})
 
 
 
