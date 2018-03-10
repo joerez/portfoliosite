9787803a37461ef4aa6007e12438617b5f723ce9
Joe Rezendes <rezenbiz@gmail.com>
2018-03-09 21:45:45 -0800

    new book

--- public/book.js
+++ public/book.js
@@ -31,15 +31,17 @@ $('.book').css("transform", "rotateY(40deg) rotateX(-39deg) scale(1.1)");
 $('.cover').on("click", function() {
 
   $('.book').css("transform", "rotateY(0deg) rotateX(-0deg) scale(1)");
+  $('.book').removeClass('hoveit');
    $('.cover').css('transform', 'rotateY(-180deg)').css("color", "brown");
    $(this).css('z-index', '20');
-   $('.firstPage').delay(100).show();
+})
+
+$('.firstPage').on("click", function() {
+
+  $(this).css('transform', 'rotateY(-180deg)');
+
 })
 
 
 
-// $('.pg1').hover(
-//        function(){ $(this).addClass('animated flip') },
-//        function(){ $(this).removeClass('animated flip') }
-// )
 })
