2dc6ed6cf96b68f0cfbf29f6e1434b008731bd13
Joe Rezendes <rezenbiz@gmail.com>
2018-03-10 18:40:04 -0800

    book going better

--- public/book.js
+++ public/book.js
@@ -1,32 +1,6 @@
 $(document).ready(function()
 {
 
-$('.pg2').on("click", function() {
-
-  $(this).css('transform', 'rotateY(-180deg)');
-
-})
-
-$('.pg3').on("click", function() {
-
-   $(this).css('transform', 'rotateY(-180deg)');
-   $(this).css('z-index', '20');
-
-})
-
-$('.pg4').on("click", function() {
-
-   $(this).css('transform', 'rotateY(-180deg)');
-   $(this).css('z-index', '20');
-
-})
-
-$('.pg5').on("click", function() {
-
-$('.book').css("transform", "rotateY(40deg) rotateX(-39deg) scale(1.1)");
-
-
-})
 
 function newPage(element) {
 
@@ -62,6 +36,7 @@ $('.bookBack').on("click", function() {
   $('.cover').css('transform', 'rotateY(0deg)');
   $('.cover').css("color", "white");
   $('.book').css("transform", "rotateY(40deg) rotateX(-39deg) scale(1.1)").show(0, function() {
+    $('')
 })
 })
 
